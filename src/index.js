73f478791c95929badca50f289b71da4342e2c52
roberthdg <roberthdgg@gmail.com>
2019-04-02 11:45:28 -0400

    módulo de prueba

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const _ = require('lodash');
+const faker = require('faker');
+let app = express();
+app.set('port', process.env.PORT || 3000);
+
+//ruta para obtener el arreglo de personas
+app.get('/people', (req, res) => { 
+    people= _.times(faker.random.number({ min: 0, max: 10 })).map(() => ({ firstName: faker.name.firstName(), lastName: faker.name.lastName() }));
+    console.log(people);
+    return(people)
+}); 
+
+app.listen(app.get('port'),() => {console.log('server on port ', app.get('port'));});
